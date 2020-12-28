10446ebe3e0c66947708a09e5a07e687f6df0566
Matt McAdams <mattgmcadams@gmail.com>
2020-12-27 23:16:46 -0500

    add random phrase parts

---
+++ README.md
@@ -0,0 +1,6 @@
+# Javascript Portfolio Project 1
+
+Plan the thing:
+
+* inspiring messages
+  * beginning, middle, end
---
+++ script.js
@@ -0,0 +1,4 @@
+const beginning = ["You're going to ", "You have to ", "Get ready to ", "Show me how you "];
+const middle = ["kill it, ", "wreck it, ", "face God, ", "be the best you, ", "rock this, ", "balance it all, "];
+const end = ["today!", "you beast!", "for sure!", "or my name's Sally! (trust me, it's not)"];
+
