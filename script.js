aa20d9c4247dce4c5979b70f878cecebe7fac2bf
Matt McAdams <mattgmcadams@gmail.com>
2020-12-27 23:49:06 -0500

    shorten function

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 var json = require('./phrases.json');
 
 const getRandomPhrase = () => {
-    const b = json.beginning[Math.floor(Math.random() * json.beginning.length)];
-    const m = json.middle[Math.floor(Math.random() * json.middle.length)];
-    const e = json.end[Math.floor(Math.random() * json.end.length)];
+    return json.beginning[Math.floor(Math.random() * json.beginning.length)] +
+        json.middle[Math.floor(Math.random() * json.middle.length)] +
+        json.end[Math.floor(Math.random() * json.end.length)];
 
-    return b + m + e;
 }
 
 console.log(getRandomPhrase());
